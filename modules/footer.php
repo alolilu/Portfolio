eb23908173f410abd19620b1b4e8a73bc6fd3ebf
Alexis FREDRIKSEN Semper Connect <33064126+alolilu@users.noreply.github.com>
2023-04-24 07:07:22 -0700

    Add files via upload

--- modules/footer.php
+++ modules/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="logo"></div>
-    <p>© 2022</p>
+    <p>© 2023</p>
     <p><?= element17 ?><span id="spanColor">Visual Studio Code</span></p>
     <p><?= element17 ?><span id="spanColor"><?= element21 ?></span></p>
     <p><?= element19 ?> <span id="spanColor">Infinity Free</span></p>
@@ -10,7 +10,8 @@
     <ul>
         <a target="_blank" href="https://www.linkedin.com/in/alexis-fredriksen-a666161b5/"><i class="fa-brands fa-linkedin"></i></a>
         <a target="_blank" href="https://github.com/alolilu"><i class="fa-brands fa-github-square"></i></a>
-        <a target="_blank" href="https://www.instagram.com/__alexiis_/?hl=fr"><i class="fa-brands fa-instagram"></i></a>
-        <a target="_blank" href=""><i class="fa-brands fa-facebook-square"></i></a>
+        <a target="_blank" href="https://www.instagram.com/websites_creation_/?hl=fr"><i class="fa-brands fa-instagram"></i></a>
+        <a target="_blank" href="tel:0781050541"><i class="fa-solid fa-square-phone"></i></a>
+        <a target="_blank" href="mailto:developpeuralexispro@gmail.com"><i class="fa-solid fa-envelope"></i></a>
     </ul>
 </div>
