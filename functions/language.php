35799bdcac27f6b88a1432296d368cf8c9e12a8f
Alexis FREDRIKSEN Semper Connect <33064126+alolilu@users.noreply.github.com>
2023-04-28 03:32:00 -0700

    Add files via upload

--- functions/language.php
+++ functions/language.php
@@ -3,9 +3,9 @@
 $lang = "fr";
 
 if(isset($_COOKIE['language']) && $_COOKIE['language'] == "en"){
-    include("traduction/fr.php");
-    $lang = "fr";
-} else {
     include("traduction/en.php");
     $lang = "en";
+} else {
+    include("traduction/fr.php");
+    $lang = "fr";
 }
